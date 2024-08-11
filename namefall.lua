f8788d77e59377b8e77551fb2009791332dac294
SellfC <110660214+SellfC@users.noreply.github.com>
2024-08-11 15:01:45 +0300

    Update namefall.lua

--- namefall.lua
+++ namefall.lua
@@ -1,1 +1,6 @@
-
+getgenv().Buy = {["Mythical"] = 5, ["Legendary"] = 5}
+getgenv().PrivateCode = "NpfQh0PmSZ"
+getgenv().Velocity = 80
+getgenv().WebHook = "https://discord.com/api/webhooks/1137986267808796723/X_RT7ofzUep_66__F3n_wBzkJJkwjQ0GvP4GFTXX2ovGzxGbD0w2q7RMhe5BFUGZOxsa"
+script_key="lMEsmwxerHxDApIOjNFBOTDUNevnWVar";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/358cf03271404d3aae83934466f19d69.lua"))()
