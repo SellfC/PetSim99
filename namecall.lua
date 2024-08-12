72370c8356b1eca2a503f3b9901c55ca45e0c074
SellfC <110660214+SellfC@users.noreply.github.com>
2024-08-12 18:46:01 +0300

    Update namecall.lua

--- namecall.lua
+++ namecall.lua
@@ -1,2 +1,6 @@
-script_key="XxYuPZAYXheZWWCwPhOxfTnbEopEDGuA";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9217a7f5a73261867db220523aeafc88.lua"))()
+--Config
+getgenv().speed = 100 -- can set 100 - 500 speed tween
+getgenv().privatecode = "NpfQh0PmSZ"
+getgenv().Webhook = ""
+--Script
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c831bed22e229ea83e8a6c43f60db04a.lua"))()
