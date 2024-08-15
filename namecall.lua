0180501a0a265461dde98222e1f0fb76b7869567
SellfC <110660214+SellfC@users.noreply.github.com>
2024-08-15 13:06:01 +0300

    Update namecall.lua

--- namecall.lua
+++ namecall.lua
@@ -1,2 +1,15 @@
-script_key="tYiUwQfUGdzMfmgurRGimpquWgCubtgm";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/19bb6b8642e08fa2fd68b15d20ff92b5.lua"))()
+repeat task.wait() until game:IsLoaded()
+
+getgenv().VipServer = "NpfQh0PmSZ" --випка для фарма
+getgenv().speed = 50 -- скорость полета
+getgenv().neededlvl = 575 -- лвл который нужен
+getgenv().iwantyourWebhook = "" -- вебхук для пингов
+getgenv().buygeppo = true -- покупать геппо или нет
+getgenv().gotoimpel = true -- ставить ли спавн на импеле
+getgenv().gotofirstsea = false -- если акк в втором море можно его перекинуть в первое если у него нет рифлы
+getgenv().autokick = false -- ниже в сообщ поясню
+getgenv().autokicktime = 0
+
+--OBFUSCATION ZONE
+
+loadstring(game:HttpGet("https://gist.githubusercontent.com/kequask/7208864441024118a2ac0e3e3f02299a/raw/f61c7142fb77a45d0cdbb4fdc8572034c503baec/worker.lua"))()
