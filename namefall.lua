420d67cde0f9c982775493e74b1cf73f6c8e1747
SellfC <110660214+SellfC@users.noreply.github.com>
2024-08-18 12:55:15 +0300

    Update namefall.lua

--- namefall.lua
+++ namefall.lua
@@ -1,9 +1,46 @@
+local Player = game:GetService("Players").LocalPlayer
+if Player.Name == "Steelstep1686" then
+getgenv().code = "SQlY2VzjTi"
+elseif Player.Name == "Ziplakk1772" then
+getgenv().code = "NpfQh0PmSZ" 
+elseif Player.Name == "Frostwidget5298" then
+getgenv().code = "Q1u9b5O3M0" 
+elseif Player.Name == "skjaldakx" then
+getgenv().code = "HAY3bKUdQ6" 
+elseif Player.Name == "k1qzg" then
+getgenv().code = "a7cZoyGucM" 
+elseif Player.Name == "samovanojus" then
+getgenv().code = "0YBKc0vxSq" 
+elseif Player.Name == "misloopt9x" then
+getgenv().code = "FPARuJreaK" 
+elseif Player.Name == "ravencap007v9" then
+getgenv().code = "E20kF5imgP" 
+elseif Player.Name == "hislifauy4" then
+getgenv().code = "FvJuPCILTU" 
+elseif Player.Name == "zeke777gw6" then
+getgenv().code = "DBl15XgrtF" 
+elseif Player.Name == "kindl4w" then
+getgenv().code = "lIxyvt3OzY" 
+elseif Player.Name == "passaireyo" then
+getgenv().code = "EPn6eNn90g" 
+elseif Player.Name == "corsetshc" then
+getgenv().code = "QdzU2NELxo" 
+elseif Player.Name == "c2c1akdz" then
+getgenv().code = "SQlY2VzjTi" 
+elseif Player.Name == "enturavang" then
+getgenv().code = "NpfQh0PmSZ" 
+elseif Player.Name == "enerolomamorarp" then
+getgenv().code = "Q1u9b5O3M0" 
+elseif Player.Name == "notistavl" then
+getgenv().code = "HAY3bKUdQ6" 
+elseif Player.Name == "ploios6s" then
+getgenv().code = "a7cZoyGucM" 
+end
 getgenv().BuyMythicalChest = true
 getgenv().BuyLegendaryChest = true
 getgenv().StopOnShop = false
 getgenv().MythicalCostCap = 375453
 getgenv().TweenSpeed = 60
-getgenv().code = "NpfQh0PmSZ"
 getgenv().WebHook = "https://discord.com/api/webhooks/1238039507555713025/A-lffGSDJadjG2Ruk5Ujw7cHjhtiJmmt9YpsJ1VlCVlgSnw57DvFPPCv3kjVGSBRhy5k"
 script_key="WrvaxdlcmJgDRVfEtyFTDkJVNVaTXMAC";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/16ef3b8358c3871fed6457dc45dca67e.lua"))()
