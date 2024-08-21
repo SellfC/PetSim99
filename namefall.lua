abc91ffabef8241224498868460493e3f6b3c1dd
SellfC <110660214+SellfC@users.noreply.github.com>
2024-08-21 20:58:22 +0300

    Update namefall.lua

--- namefall.lua
+++ namefall.lua
@@ -44,7 +44,7 @@ getgenv().BuyMythicalChest = true
 getgenv().BuyLegendaryChest = true
 getgenv().StopOnShop = false
 getgenv().MythicalCostCap = 375453
-getgenv().TweenSpeed = 60
+getgenv().TweenSpeed = 100
 getgenv().WebHook = "https://discord.com/api/webhooks/1238039507555713025/A-lffGSDJadjG2Ruk5Ujw7cHjhtiJmmt9YpsJ1VlCVlgSnw57DvFPPCv3kjVGSBRhy5k"
 script_key="WrvaxdlcmJgDRVfEtyFTDkJVNVaTXMAC";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/16ef3b8358c3871fed6457dc45dca67e.lua"))()
