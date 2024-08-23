8a195274cc7f29b878cf3212eb05768df3d60b2f
SellfC <110660214+SellfC@users.noreply.github.com>
2024-08-23 21:25:46 +0300

    Update namecall.lua

--- namecall.lua
+++ namecall.lua
@@ -1,2 +1,42 @@
-script_key="tYiUwQfUGdzMfmgurRGimpquWgCubtgm";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/19bb6b8642e08fa2fd68b15d20ff92b5.lua"))()
+repeat task.wait() until game:IsLoaded()
+local Player = game:GetService("Players").LocalPlayer
+
+if Player.Name == "openteacherbd" then
+    getgenv().VipServer = "SQlY2VzjTi"
+    elseif Player.Name == "kultnogve" then
+    getgenv().VipServer = "NpfQh0PmSZ"
+    elseif Player.Name == "esplorahotelxd" then
+    getgenv().VipServer = "Q1u9b5O3M0"
+    elseif Player.Name == "faloni5v" then
+    getgenv().VipServer = "HAY3bKUdQ6"
+    elseif Player.Name == "2zmbgm" then
+    getgenv().VipServer = "a7cZoyGucM"
+    elseif Player.Name == "clutzypiemc" then
+    getgenv().VipServer = "0YBKc0vxSq"
+    elseif Player.Name == "bouveret4g" then
+    getgenv().VipServer = "FPARuJreaK"
+    elseif Player.Name == "dedel24oo" then
+    getgenv().VipServer = "E20kF5imgP"
+    elseif Player.Name == "gmaszyskoye" then
+    getgenv().VipServer = "FvJuPCILTU"
+    elseif Player.Name == "Zapstrik3511" then
+    getgenv().VipServer = "DBl15XgrtF"
+    elseif Player.Name == "Overfuz5674" then
+    getgenv().VipServer = "lIxyvt3OzY"
+    elseif Player.Name == "Crutfu4619" then
+    getgenv().VipServer = "EPn6eNn90g"
+    elseif Player.Name == "Hronshar5554" then
+    getgenv().VipServer = "QdzU2NELxo"
+end
+getgenv().speed = 50 -- скорость полета
+getgenv().neededlvl = 575 -- лвл который нужен
+getgenv().iwantyourWebhook = "" -- вебхук для пингов
+getgenv().buygeppo = true -- покупать геппо или нет
+getgenv().gotoimpel = true -- ставить ли спавн на импеле
+getgenv().gotofirstsea = false -- если акк в втором море можно его перекинуть в первое если у него нет рифлы
+getgenv().autokick = true -- ниже в сообщ поясню
+getgenv().autokicktime = 600
+
+--OBFUSCATION ZONE
+
+loadstring(game:HttpGet("https://gist.githubusercontent.com/kequask/585acd10b20ef2a59f08afafb547a9cd/raw/5e612b192d6396b639ebf5d2d52c22db6be59c3d/carcat.lua"))()
